dd1af92722ef8a6fbcc3d9ff8c5cbb84ccf15812
LouisLEBG <louis.maze49@gmail.com>
2021-09-29 15:29:06 +0200

    Condition connection

--- bdd/userConnexion.php
+++ bdd/userConnexion.php
@@ -6,19 +6,19 @@ include 'dbConnexion.php';
 $login = $_POST["login"];
 $mdp = $_POST["mdp"];
 
-$stmt = $conn->prepare("SELECT user, mdp FROM utilisateur");
+$stmt = $conn->prepare("SELECT user, mdp FROM utilisateur where user = '".$login."';");
 $stmt->execute();
 
 foreach ($stmt as $row){
     if ($login==$row['user'] && $mdp==$row['mdp']){
-        header('Location: ./public/welcome.php ');
+        header('Location: ../public/welcome.php ');
         $_SESSION['user']=$login;
     }
     elseif ($login!=$row['user']){
-        header('Location: ./public/inscription.php');
+        header('Location: ../public/inscription.php');
     }
     else{
-        header('Location: ./public/Connexion.php');
+        header('Location: ../public/Connexion.php');
     }
 }
 ?>
